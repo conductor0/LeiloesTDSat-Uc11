f3d1f97490b0318d62e4a31f2296d31e970e8056
conductor0 <danielnordio6@gmail.com>
2025-03-28 01:44:27 -0300

    Metodo de listagem em funcionamento

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -14,21 +14,22 @@ import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class ProdutosDAO {
     
     Connection conn;
-    PreparedStatement prep;
+    PreparedStatement stmt;
     ResultSet resultset;
-    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
+
     public void cadastrarProduto (ProdutosDTO produto){
         
         
         conn = new conectaDAO().connectDB();
-        
-        PreparedStatement stmt = null;
         
         try {
             stmt = conn.prepareStatement("INSERT INTO produtos (nome,valor,status)VALUES(?,?,?)");
@@ -46,13 +47,31 @@ public class ProdutosDAO {
         
     }
     
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
-    }
-    
-    
-    
-        
+    public List<ProdutosDTO> listarProdutos(){
+
+        try {
+            conn = new conectaDAO().connectDB();
+            
+            String sql;
+            sql = "SELECT * FROM uc11.produtos;";
+            stmt = conn.prepareStatement(sql);
+            resultset = stmt.executeQuery(sql);
+            
+            List<ProdutosDTO> lista = new ArrayList<>();
+            
+            while(resultset.next()){
+                ProdutosDTO produtosDTO = new ProdutosDTO();
+                produtosDTO.setId(resultset.getInt(1));
+                produtosDTO.setNome(resultset.getString(2));
+                produtosDTO.setValor(resultset.getInt(3));
+                produtosDTO.setStatus(resultset.getString(4));
+
+                lista.add(produtosDTO);
+            }
+            return lista;
+        } catch (SQLException ex) {
+            return null;
+        }
+    }      
 }
 
